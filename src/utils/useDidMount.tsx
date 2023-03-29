af424d4e3b5ce37a6ef5c195e3d96e35f4dca7a9
Daniel Ladrón de Guevara <daniel.ldg@icloud.com>
2023-03-29 08:48:03 -0600

    hooks hechos para el proyecto

---
+++ src/utils/useDidMount.tsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+
+export const useDidMount = () => {
+	const [didMount, setDidMount] = useState(false);
+	useEffect(() => {
+		setDidMount(true);
+	}, []);
+
+	return didMount;
+};
---
+++ src/utils/useIsMobile.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export const useIsMobile = () => {
+	const [width, setWidth] = useState<number>(window.innerWidth);
+
+	const handleWindowSizeChange = () => setWidth(window.innerWidth);
+
+	useEffect(() => {
+		window.addEventListener("resize", handleWindowSizeChange);
+		return () => {
+			window.removeEventListener("resize", handleWindowSizeChange);
+		};
+	}, []);
+
+	return width < 768;
+};
