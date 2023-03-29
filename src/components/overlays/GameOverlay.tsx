f3b657c708d57bc9e623332b609c710dcaee06a0
Daniel Ladrón de Guevara <daniel.ldg@icloud.com>
2023-03-29 09:34:31 -0600

    Se agregan mas frases al ganar o perder en overlay

--- src/components/overlays/GameOverlay.tsx
+++ src/components/overlays/GameOverlay.tsx
@@ -15,19 +15,26 @@ const GameOverlay = ({ isCompleted, isLoss }: IProps) => {
 	const didMount = useDidMount();
 	const [showCompleted, setShowCompleted] = useState(false);
 	const [showLoss, setShowLoss] = useState(false);
+	const [messages, setMessages] = useState({ success: getRandom(success), fail: getRandom(fail) });
 
 	useEffect(() => {
 		if (didMount && !previousState?.isCompleted && isCompleted) {
 			setTrueTimeout(setShowCompleted, 3000);
+			setTimeout(() => {
+				setMessages(prev => ({ ...prev, success: getRandom(success) }));
+			}, 4000);
 		} else if (didMount && !previousState?.isLoss && isLoss) {
 			setTrueTimeout(setShowLoss, 3000);
+			setTimeout(() => {
+				setMessages(prev => ({ ...prev, fail: getRandom(fail) }));
+			}, 4000);
 		}
 	}, [isCompleted, isLoss]);
 
 	return (
 		<div style={{ position: "absolute", width: "100vw", height: "100vh", zIndex: "100", pointerEvents: "none" }}>
-			<TextAnimation show={showCompleted} text={["¡Ganaste!"]} lottieData={confettiData} />
-			<TextAnimation show={showLoss} text={["Intentalo", "otra vez"]} lottieData={smokeData} />
+			<TextAnimation show={showCompleted} text={messages.success} lottieData={confettiData} />
+			<TextAnimation show={showLoss} text={messages.fail} lottieData={smokeData} />
 		</div>
 	);
 };
@@ -38,5 +45,42 @@ const setTrueTimeout = (dispatch: React.Dispatch<React.SetStateAction<boolean>>,
 		dispatch(false);
 	}, timeout);
 };
+
+const success: string[][] = [
+	["¡Felicidades!"],
+	["¡Eres un campeón!"],
+	["¡Ganador!"],
+	["¡Lo lograste!"],
+	["¡Bravo!"],
+	["¡Excelente!"],
+	["¡Increíble!"],
+	["¡Fantástico!"],
+	["¡Espectacular!"],
+	["¡Impresionante!"],
+	["¡Perfecto!"],
+	["¡Genial!"],
+	["¡Asombroso!"],
+	["¡Victoria!"],
+	["¡Eres el mejor!"],
+	["¡Superaste", "el desafío!"],
+	["¡Eres imparable!"],
+	["¡Ganaste!"],
+];
+
+const fail: string[][] = [
+	["Inténtalo de nuevo"],
+	["Lo siento", "perdiste"],
+	["Vuelve a", "intentarlo"],
+	["No te rindas"],
+	["Prueba otra vez"],
+	["Mejor suerte", "la próxima"],
+	["No te desanimes"],
+	["Ups, fallaste"],
+	["No te preocupes", "sigue intentándolo"],
+	["Mala suerte"],
+	["Intentalo", "otra vez"],
+];
+
+const getRandom = (list: any[]) => list[Math.floor(Math.random() * list.length)];
 
 export default GameOverlay;
